9a4cc79d2d64ef72a6a191dd8953e1bc3113d791
Marcelo Pereira <marcelo12tab@gmail.com>
2021-03-31 16:12:53 +0100

    add endpoint to list all books

--- src/main/java/com/cdc/books/controllers/BookController.java
+++ src/main/java/com/cdc/books/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.cdc.books.controllers;
 import com.cdc.books.controllers.requests.BookRequest;
 import com.cdc.books.entities.Book;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class BookController {
@@ -24,6 +26,13 @@ public class BookController {
         Book book = bookRequest.toModel(this.entityManager);
         this.entityManager.persist(book);
         return ResponseEntity.ok(book);
+    }
+
+    @GetMapping("/books")
+    @Transactional
+    public ResponseEntity<List<Book>> list() {
+        List<Book> books = this.entityManager.createQuery("from " + Book.class.getName(), Book.class).getResultList();
+        return ResponseEntity.ok(books);
     }
 
 }
